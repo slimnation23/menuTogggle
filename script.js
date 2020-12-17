191a62bbbf90bfba1bbac70bca6ca2c0fe0ea557
slimnation23 <slimnation2@gmail.com>
2020-12-18 00:54:48 +0200

    done

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    let menuToggle = document.querySelector('.toggle')
+    let navigation = document.querySelector('.navigation')
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+
+}
